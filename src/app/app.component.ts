a8ed71ef1e830a8e1a9a8739d7f9964c1347fe7d
vovapankiw <vovapankiw@gmail.com>
2021-12-11 20:09:15 +0200

    added func in component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   title = 'hotjar';
+
+  constructor(@Inject(DOCUMENT) private document: Document,) {
+
+  }
+
+  ngAfterContentInit(): void {
+    ((h, o, t, j, a, r) => {
+      h.hj =
+        h.hj ||
+        /* tslint:disable:only-arrow-functions */
+        function () {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid:2743639,hjsv:6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window as any, this.document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+  }
 }
